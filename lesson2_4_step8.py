015376604f1b38ad3355b0bc23be63237718db98
MarinaBorovaya <borovaia.marina@gmail.com>
2020-01-10 17:22:38 +0300

    add lesson2.4 step8

---
+++ lesson2_4_step8.py
@@ -0,0 +1,42 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+import math
+import time
+
+def calc(x):
+	return str(math.log(abs(12*math.sin(int(x)))))
+
+browser = webdriver.Chrome()
+
+browser.get("http://suninjuly.github.io/explicit_wait2.html")
+
+try:
+
+	# говорим Selenium проверять в течение 20 секунд
+	price = WebDriverWait(browser, 20).until(
+			EC.text_to_be_present_in_element((By.ID, "price"), "$100")
+		)
+	button1 = browser.find_element_by_id("book")
+	button1.click()
+
+	browser.execute_script("return arguments[0].scrollIntoView(true);", button1)
+
+	x_element = browser.find_element_by_css_selector("#input_value")
+	x = x_element.text
+
+	y = calc(x)
+
+	input1 = browser.find_element_by_css_selector("#answer")
+	input1.send_keys(y)
+
+	button2 = browser.find_element_by_css_selector("#solve")
+	button2.click()
+
+finally:
+
+	# ожидание чтобы визуально оценить результаты прохождения скрипта
+	time.sleep(10)
+	# закрываем браузер после всех манипуляций
+	browser.quit()
